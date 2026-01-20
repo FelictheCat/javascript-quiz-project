d3c1286dfc1f5360d19b87292dede0b9ca7df11d
Felix GW <f_gud_will@hotmail.co.uk>
2026-01-20 01:36:49 +0100

    Nearly finished day, just checking git is work

--- src/question.js
+++ src/question.js
@@ -1,7 +1,20 @@
 class Question {
     // YOUR CODE HERE:
-    //
     // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
     // 2. shuffleChoices()
+      shuffleChoices() {
+        for (let i = 0; i < this.choices.length; i++) {
+          const randomIndex = Math.floor(Math.random() * this.choices.length);
+          const temp = this.choices[i];
+          this.choices[i] = this.choices[randomIndex];
+          this.choices[randomIndex] = temp;
+          }
+        } 
 }
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,35 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
+        // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    } 
     // 4. shuffleQuestions()
-
+    shuffleQuestions() {{
+        for (let i = 0; i < this.questions.length; i++) {
+          const randomIndex = Math.floor(Math.random() * this.questions.length);
+          const temp = this.questions[i];
+          this.questions[i] = this.questions[randomIndex];
+          this.questions[randomIndex] = temp;
+        }
+      }
+    }
     // 5. checkAnswer(answer)
+    checkAnswer(){
+        
+    }
 
     // 6. hasEnded()
 }
